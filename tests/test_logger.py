66f7d203e1caf407394630589d876982363e62aa
anhtu duong <anhtu.duong@studenti.unitn.it>
2023-05-04 15:13:50 +0200

    config init tests

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -6,18 +6,6 @@
 @date 2023-05-04
 """
 
-# Resolve path
-from pathlib import Path
-import sys
-import os
-
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[1]
-VISION_PATH = os.path.abspath(os.path.join(ROOT, "vision"))
-if VISION_PATH not in sys.path:
-    sys.path.append(VISION_PATH)  # add VISION_SCRIPTS_PATH to PATH
-VISION_PATH = Path(os.path.relpath(VISION_PATH, Path.cwd()))  # relative
-
 # Import
 import unittest
 from vision.scripts.utils.Logger import Logger
