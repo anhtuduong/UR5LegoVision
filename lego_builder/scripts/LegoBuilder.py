d41c688ba08589934f290bd3723a3da8ead4aa2f
anhtu duong <anhtu.duong@studenti.unitn.it>
2023-11-19 15:38:35 +0100

    design lego castle

--- lego_builder/scripts/LegoBuilder.py
+++ lego_builder/scripts/LegoBuilder.py
@@ -18,7 +18,8 @@ if str(ROOT) not in sys.path:
 BUTTON_IMG_PATH = os.path.abspath(os.path.join(ROOT, "lego_builder/button_img"))
 OUTPUT_PATH = os.path.abspath(os.path.join(ROOT, "lego_builder/output"))
 BUTTON_IMG_PX = 30
-IMG_PX = 100
+IMG_PX = 200
+SCALE_FACTOR = 2
 
 import tkinter as tk
 from PIL import Image, ImageTk
@@ -61,26 +62,45 @@ class LegoBuilder:
         # Bind the mouse wheel to the button_frame for scrolling
         button_frame.bind("<MouseWheel>", lambda event, canvas=canvas: self.on_mousewheel(event, canvas))
 
+        # Create the buttons
         block_names = BLOCK_NAMES
         self.block_buttons = []
-        for block_name in block_names:
-            block_image = Image.open(f"{BUTTON_IMG_PATH}/{block_name}.png")
-            # block_image = block_image.resize((RESIZE_PX, RESIZE_PX), Image.LANCZOS)
-            # pil_image = block_image.copy()  # Make a copy of the PIL image
+        buttons_per_row = 2  # Number of buttons per row
 
+        for idx, block_name in enumerate(block_names):
+            if idx % buttons_per_row == 0:
+                button_row = tk.Frame(frame_buttons)
+                button_row.pack(side=tk.TOP)
+
+            block_image = Image.open(f"{BUTTON_IMG_PATH}/{block_name}.png")
             button_block_img = block_image.copy()
             button_block_img = button_block_img.resize((BUTTON_IMG_PX, BUTTON_IMG_PX), Image.LANCZOS)
             button_tk_image = ImageTk.PhotoImage(button_block_img)
-            block_button = tk.Button(frame_buttons, text=block_name, image=button_tk_image, compound=tk.TOP,
-                                     command=lambda img=block_image.copy(): self.select_block(img))
+
+            block_button = tk.Button(button_row, text=block_name, image=button_tk_image, compound=tk.TOP,
+                                    command=lambda img=block_image.copy(): self.select_block(img))
             block_button.image = button_tk_image
-            block_button.pack(side=tk.TOP, padx=5, pady=5)
+            block_button.pack(side=tk.LEFT, padx=5, pady=5)
+
             self.block_buttons.append(block_button)
 
         self.canvas = tk.Canvas(self.root, width=500, height=500, bg="white")
         self.canvas.pack(side=tk.LEFT, padx=10, pady=10)
         self.canvas.bind("<Button-1>", self.place_block)
         self.canvas.bind("<Button-3>", self.delete_block)
+
+        # Create frame for right-side buttons
+        right_button_frame = tk.Frame(self.root)
+        right_button_frame.pack(side=tk.LEFT, padx=1, pady=10, anchor=tk.NE)
+
+        rotate_button = tk.Button(right_button_frame, text="Rotate", command=self.rotate_block)
+        rotate_button.pack(side=tk.TOP, padx=10, pady=5)
+
+        flip_horizontal_button = tk.Button(right_button_frame, text="Flip Horizontal", command=self.flip_horizontal)
+        flip_horizontal_button.pack(side=tk.TOP, padx=10, pady=5)
+
+        flip_vertical_button = tk.Button(right_button_frame, text="Flip Vertical", command=self.flip_vertical)
+        flip_vertical_button.pack(side=tk.TOP, padx=10, pady=5)
 
         export_button = tk.Button(self.root, text="Export Image", command=self.export_image)
         export_button.pack(side=tk.BOTTOM, padx=10, pady=10)
@@ -135,23 +155,51 @@ class LegoBuilder:
             y -= block_height / 2  # Adjust y-coordinate to center the image
             self.canvas_items[self.canvas.create_image(x, y, anchor=tk.NW, image=self.current_block, tags="added_block")] = block_image
 
+    def rotate_block(self):
+        if self.ghost_block:
+            self.canvas.delete(self.ghost_block)
+            self.pil_image = self.pil_image.rotate(90)
+            self.current_block = ImageTk.PhotoImage(self.pil_image)
+            self.ghost_block = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.current_block, tags="ghost_block")
+            self.canvas.tag_lower(self.ghost_block)  # Ensure ghost block stays at the bottom
+
+    def flip_horizontal(self):
+        if self.ghost_block:
+            self.canvas.delete(self.ghost_block)
+            self.pil_image = self.pil_image.transpose(Image.FLIP_LEFT_RIGHT)
+            self.current_block = ImageTk.PhotoImage(self.pil_image)
+            self.ghost_block = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.current_block, tags="ghost_block")
+            self.canvas.tag_lower(self.ghost_block)
+
+    def flip_vertical(self):
+        if self.ghost_block:
+            self.canvas.delete(self.ghost_block)
+            self.pil_image = self.pil_image.transpose(Image.FLIP_TOP_BOTTOM)
+            self.current_block = ImageTk.PhotoImage(self.pil_image)
+            self.ghost_block = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.current_block, tags="ghost_block")
+            self.canvas.tag_lower(self.ghost_block)
+
     def delete_block(self, event):
         items = self.canvas.find_closest(event.x, event.y)
         if "ghost_block" not in self.canvas.gettags(items):
             self.canvas.delete(items)
 
     def export_image(self):
-        canvas_image = Image.new("RGBA", (self.canvas.winfo_width(), self.canvas.winfo_height()), (255, 255, 255, 255))
+        canvas_width = self.canvas.winfo_width() * SCALE_FACTOR
+        canvas_height = self.canvas.winfo_height() * SCALE_FACTOR
+        canvas_image = Image.new("RGBA", (canvas_width, canvas_height), (255, 255, 255, 255))
 
         # Get the canvas background color (white) and paste it onto the canvas image
-        canvas_bg = Image.new("RGB", (self.canvas.winfo_width(), self.canvas.winfo_height()), "white")
+        canvas_bg = Image.new("RGB", (canvas_width, canvas_height), "white")
         canvas_image.paste(canvas_bg, (0, 0))
 
         # Iterate through canvas items and export them onto the canvas image
         for item, block_image in self.canvas_items.items():
             x0, y0 = self.canvas.coords(item)
+            x0 *= SCALE_FACTOR
+            y0 *= SCALE_FACTOR
             block_image = block_image.pil_image
-            block_image = block_image.resize((IMG_PX, IMG_PX), Image.LANCZOS)
+            block_image = block_image.resize((IMG_PX * SCALE_FACTOR, IMG_PX * SCALE_FACTOR), Image.LANCZOS)
 
             # Get the alpha channel from the block image
             alpha = block_image.getchannel('A')
